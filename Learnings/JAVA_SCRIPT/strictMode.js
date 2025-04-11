3be5d1807cf90bd8e14ea4538de297468607fca8
PavithraRaviCSE <pavithraravicse@gmail.com>
2025-04-11 17:41:41 +0530

    strict example

---
+++ Learnings/JAVA_SCRIPT/strictMode.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let a = 10;
+console.log('̥a: ' , a);
+
+let b = 10;
+
+console.log('̥a==b  a===b', a==b , " " , a === b);
+
+
+// JavaScript Learning Path						
+						
+//  1. Introduction				Reference Links:		
+// - An Introduction				https://javascript.info/		
+// - An Introduction to JavaScript				https://developer.mozilla.org/en-US/docs/Web/JavaScript		
+// - Manuals and specifications				https://www.w3schools.com/js/		
+// - Code editors				https://www.tutorialspoint.com/javascript/index.htm		
+// - Developer console				https://eloquentjavascript.net/		
+// 				https://www.youtube.com/watch?v=hdI2bqOjy3c&t=2s&ab_channel=TraversyMedia		
+//  2. JavaScript Fundamentals						
+// - Hello, world!						
+// - Code structure						
+// - The modern mode, "use strict"				Preferable Link:		
+// - Variables				https://javascript.info/		
+// - Data types						
+// - Interaction: alert, prompt, confirm						
+// - Type Conversions		
