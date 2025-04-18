076c7c18fc7be93bd3717a545f140637feae6fc2
PavithraRaviCSE <pavithraravicse@gmail.com>
2025-04-18 18:29:21 +0530

    methods of primitives

---
+++ Learnings/JAVA_SCRIPT/MethodsOfPrimitive.js
@@ -0,0 +1,17 @@
+const name = "Nive";
+
+// console.log('̥name.toUpperCase()', name.toUpperCase(), name.toUpperCase(), name.toString(), name.substring(1,3), name.satrtWith("N"));const name = "Nive";
+
+console.log("name.toUpperCase():", name.toUpperCase());
+console.log("name.toLowerCase():", name.toLowerCase());
+console.log("name.toString():", name.toString());
+console.log("name.substring(1, 3):", name.substring(1, 3));
+console.log("name.startsWith('N'):", name.startsWith("N"));
+console.log("name.endsWith('e'):", name.endsWith("e"));
+console.log("name.includes('iv'):", name.includes("iv"));
+console.log("name.charAt(2):", name.charAt(2));
+console.log("name.length:", name.length);
+console.log("name.repeat(2):", name.repeat(2));
+console.log("name.padStart(6, '*'):", name.padStart(6, '*'));
+console.log("name.padEnd(6, '*'):", name.padEnd(6, '*'));
+console.log("name.slice(1, 3):", name.slice(1, 3));
