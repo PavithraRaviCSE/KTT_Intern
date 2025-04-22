b81b02294c6e68b260b479cb938673bb06cc4813
PavithraRaviCSE <pavithraravicse@gmail.com>
2025-04-22 15:28:37 +0530

    users table creation

---
+++ Learnings/Node/postgres connection.sql
@@ -0,0 +1,19 @@
+-- select * from student;
+
+-- alter table student add COLUMN name varchar(100) ;
+
+-- CREATE TABLE "User" (
+--   username VARCHAR(255) UNIQUE NOT NULL,
+--   password VARCHAR(255) NOT NULL
+-- );
+
+-- drop table "User";
+
+-- CREATE TABLE "User" (
+--   id serial PRIMARY KEY,
+--   username VARCHAR(255) UNIQUE NOT NULL,
+--   password VARCHAR(255) NOT NULL
+-- );
+
+select * from "Users";
+select * from "Products";
