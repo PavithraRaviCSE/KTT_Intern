5ec03febd46ff8128bab3cbec26b7a1527292017
PavithraRaviCSE <pavithraravicse@gmail.com>
2025-05-15 19:04:28 +0530

    qoheader modal creation

---
+++ QuotationTracker/models/qoHeader.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const QoHeader = sequelize.define('QoHeader', {
+        qoNumber: {
+            type: DataTypes.BIGINT,
+            allowNull: false
+        },
+        qoDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }, 
+        expiryDate: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        subTotal: {
+            type: DataTypes.DECIMAL(12, 2),
+            allowNull: false
+        },
+        taxAmount: {
+            type: DataTypes.DECIMAL(12, 2),
+            allowNull: false
+        },
+        gstAmount: {
+            type: DataTypes.DECIMAL(12, 2),
+            allowNull: false
+        },
+        discount: {
+            type: DataTypes.DECIMAL(12, 2),
+            allowNull: false
+        },
+        totalAmount: {
+            type: DataTypes.DECIMAL(12, 2),
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1
+        },
+        user: {
+            type: DataTypes.JSONB,
+            allowNull: false
+        }
+    });
+
+    QoHeader.associate = function (models) {
+        QoHeader.belongsTo(models.Customer);
+        QoHeader.hasMany(models.QoDetail);
+    };
+
+    return QoHeader;
+};
