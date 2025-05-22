c7c829958bff7b5d9aa2dd74bb02a50d82253224
PavithraRaviCSE <pavithraravicse@gmail.com>
2025-05-22 18:24:13 +0530

    files are deleted

--- QuotationTracker/models/customer.js
+++ QuotationTracker/models/customer.js
@@ -1,34 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
     const Customer = sequelize.define('Customer', {
         name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        companyName: {
-
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(255),
             allowNull: false
         },
         phone: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(15),
             allowNull: false,
             unique: true
         },
         email: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(100),
             allowNull: false,
             unique: true
         },
+        gstNumber: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
         address: {
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1, // 1 - Active, 2 - Inactive
             allowNull: false
         }
-
     });
 
-    UserRole.associate = function(models) {
-        UserRole.hasMany(models.QuotationHeader);
-      };
+    Customer.associate = function (models) {
+        Customer.hasMany(models.QoHeader);
+    };
 
-      return Customer;
+    return Customer;
 };
